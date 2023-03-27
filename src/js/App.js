e0aa190b883a0f26bc2a8470041a23bc19509e94
Bartosz Skrzydelski <brtskr1503@gmail.com>
2023-03-27 08:57:19 +0200

    Small refactor
    -Removed dist folder and added it into gitignore
    -Small refactor to App.js
    -Updated parcel to version ^2
    -Removed parcel-bundler
    -Small changes here and there

--- src/js/App.js
+++ src/js/App.js
@@ -1,106 +1,84 @@
 import * as bootstrap from "bootstrap";
 import * as leaflet from "leaflet";
 
-const fetchInfo = (IP_API) => {
-  const ip = document.querySelector("[data-ip]");
-  const location = document.querySelector("[data-location]");
-  const timezone = document.querySelector("[data-timezone]");
-  const isp = document.querySelector("[data-isp]");
-  //function responsible for getting data from api
-  const FETCH_GEO_API = () => {
-    fetch(IP_API)
-      .then((res) => {
-        const responseJSON = res.json();
+const ipEl = document.querySelector("[data-ip]");
+const locationEl = document.querySelector("[data-location]");
+const timezoneEl = document.querySelector("[data-timezone]");
+const ispEl = document.querySelector("[data-isp]");
+const mapWrapperEl = document.querySelector("#map-holder");
 
-        if (res.ok) {
-          //if response is ok then return responseJSON variable
-          return responseJSON;
-        }
-        if (!res.ok) {
-          errorPopup.classList.add('error-popup-show');
-          //I don't have to comment this
-          console.log("something fooked up");
-          
-          return;
-        }
-      })
-      .then((proc) => {
-        ip.textContent = proc.ip;
-        location.textContent = `${proc.country} ${proc.region} ${proc.city}`;
-        timezone.textContent = proc.timezone;
-        isp.textContent = proc.org;
-        
+const greenIcon = L.icon({
+  iconUrl: "icon-location.svg",
+  iconSize: [40, 50],
+  iconAnchor: [10, 0],
+});
 
-        let coords = proc.loc.split(',')
-        let lat = coords[0]; //get only lat from API response
-        let long = coords[1]; //get only long from API response
-        console.log(lat, long)
-        console.log(proc);
-        loadMap(lat, long);
-      });
-  };
+const fetchInfo = async (IP_API) => {
+  try {
+    const res = await fetch(IP_API);
+    const data = await res.json();
 
-  const loadMap = (lat, long) => {
-    var greenIcon = L.icon({
-      iconUrl: "icon-location.svg",
-      iconSize: [40, 50], // size of the icon
-      iconAnchor: [10, 0], // point of the icon which will correspond to marker's location
-    });
-    const mapWrapper = document.querySelector("#map-holder"); //Element that holds map
-    const currentMap = document.querySelector("#map"); //Already created map
-    const cloneMap = document.createElement("div"); //creating new element that is going to be map
+    ipEl.textContent = data.ip;
+    locationEl.textContent = `${data.country} ${data.region} ${data.city}`;
+    timezoneEl.textContent = data.timezone;
+    ispEl.textContent = data.org;
 
-    cloneMap.setAttribute("id", "map"); //Adding attributes to new map
-    mapWrapper.removeChild(currentMap); //Removing old map
-    mapWrapper.appendChild(cloneMap); //Creating new map
-
-    var map = L.map("map").setView([lat, long], 13); //Adding properties for map
-
-    var marker = L.marker([lat, long], {
-      icon: greenIcon,
-    });
-    marker.addTo(map);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoiYnJ0c2tyIiwiYSI6ImNremxrcGNwcjExbzMyc2tlNmM3bDM3bm0ifQ.st5jESiVvUSdZtVCEoBXzg",
-      }
-    ).addTo(map);
-  };
-
-  FETCH_GEO_API(); //init
+    const coords = data.loc.split(",");
+    const lat = coords[0];
+    const long = coords[1];
+    loadMap(lat, long);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-//on click retrieves data from IP API
+const loadMap = (lat, long) => {
+  const currentMapEl = document.querySelector("#map");
+  const newMapEl = document.createElement("div");
+
+  newMapEl.setAttribute("id", "map");
+  mapWrapperEl.removeChild(currentMapEl);
+  mapWrapperEl.appendChild(newMapEl);
+
+  const map = L.map("map").setView([lat, long], 13);
+
+  const marker = L.marker([lat, long], {
+    icon: greenIcon,
+  });
+  marker.addTo(map);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoiYnJ0c2tyIiwiYSI6ImNremxrcGNwcjExbzMyc2tlNmM3bDM3bm0ifQ.st5jESiVvUSdZtVCEoBXzg",
+    }
+  ).addTo(map);
+};
+
 document.querySelector("[data-fetch]").addEventListener("click", () => {
-  let ipAddress = document.querySelector("[data-inputVal]");
-  let IP_API = `https://ipinfo.io/${ipAddress.value}?token=98b8269e8d5aa2`;
-
+  const ipAddress = document.querySelector("[data-inputVal]");
+  const IP_API = `https://ipinfo.io/${ipAddress.value}?token=98b8269e8d5aa2`;
   fetchInfo(IP_API);
 });
 
-//load defaults at load (users ip)
-addEventListener("load", () => {
-  let IP_API = `https://ipinfo.io/?token=98b8269e8d5aa2`;
+window.addEventListener("load", () => {
+  const IP_API = `https://ipinfo.io/?token=98b8269e8d5aa2`;
   fetchInfo(IP_API);
 });
-//Changes visiblity of dropdown with information about ip, location etc...
+
 document.querySelector(".hide-button").addEventListener("click", () => {
   document.querySelector("[data-information]").classList.toggle("maxer");
   document.querySelector(".hide-icon").classList.toggle("rotate-z-90");
 });
 
-
-//displays errorPopup (Please go back and check API response to see where 'error-popup-show' is being added)
 const errorPopup = document.querySelector(".error-popup");
-errorPopup.addEventListener('click', () => {
-  errorPopup.classList.remove('error-popup-show');
-})
+errorPopup.addEventListener("click", () => {
+  errorPopup.classList.remove("error-popup-show");
+});
